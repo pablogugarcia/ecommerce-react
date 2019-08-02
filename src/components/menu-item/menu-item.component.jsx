312b5a1d5941ff363d260f59300dbb159515212f
PabloGarcia <pablo.garcia366@gmail.com>
2019-08-02 14:33:16 -0300

    Rutas bascias de navegacion con withRouter en el menuItem

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
 
     <div
-        className={`${size} menu-item`}>
-        <div 
-        className='background-image'
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }} />
+        className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+        <div
+            className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }} />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>Comprar ahora</span>
@@ -18,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
 
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
