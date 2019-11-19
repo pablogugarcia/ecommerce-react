8add45a1c5d18bb77012e40952833fa2802d3c10
Pablo Garcia <pablo.garcia366@gmail.com>
2019-11-18 23:35:51 -0300

    HOC withSpinner component make

---
+++ src/components/with-spinner/with-spinner.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
+
+// const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
+//     return isLoading ? (
+//         <SpinnerOverlay>
+//             <SpinnerContainer>
+
+//             </SpinnerContainer>
+
+//         </SpinnerOverlay>
+//     ) :
+//         (<WrappedComponent {...otherProps} />)
+// }
+
+
+const WithSpinner = WrappedComponent => {
+
+    const Spinner = ({ isLoading, ...otherProps }) => {
+        return isLoading ? (
+            <SpinnerOverlay>
+                <SpinnerContainer>
+                </SpinnerContainer>
+            </SpinnerOverlay>
+        ) :
+            (<WrappedComponent {...otherProps} />)
+    }
+    return Spinner;
+}
+
+
+export default WithSpinner;
