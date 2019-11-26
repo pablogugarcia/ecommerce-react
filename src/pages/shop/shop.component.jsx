fe57da0d42ac16fbd91c53de3f2ea2c417b4f6a6
Pablo Garcia <pablo.garcia366@gmail.com>
2019-11-25 22:38:54 -0300

    component with spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,16 +3,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
 import { updateCollections } from '../../redux/shop/shop.actions';
 import CollectionPage from '../collection/collection';
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
 
     unsubscribeFromAuth = null;
-
-    constructor(props) {
-        super(props);
+    state = {
+        loading: true
     }
 
     componentDidMount() {
@@ -22,15 +25,17 @@ class ShopPage extends React.Component {
         collectionsRef.onSnapshot(async snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
+            this.setState({ loading: false });
         })
     }
 
     render() {
         const { match } = this.props;
+        const { loading } = this.state;
         return (
             <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionsOverview} />
-                <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+                <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={loading} {...props} />} />
+                <Route exact path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
             </div>
         )
     }
