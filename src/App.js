f3bd742307f3e90dc7f0ebf86bb7d7b58c3def46
Pablo Garcia <pablo.garcia366@gmail.com>
2019-12-07 11:52:07 -0300

    media query agregada al estilo global

--- src/App.js
+++ src/App.js
@@ -1,33 +1,21 @@
-import React from 'react';
-// Estilos
-import './App.css';
-
-// Route React 
-import { Route, Switch, Redirect } from 'react-router-dom';
-
-// Redirect nos permite hacer que no se pueda acceder a la pagina sin logearse
-
-import { connect } from 'react-redux';
-
-import { createStructuredSelector } from 'reselect';
-import { selectCurrentUser } from './redux/user/user.selectors';
-
-// Componentes
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
-import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
-import CheckoutPage from './pages/checkout/checkout.component';
-
-import Header from './components/header/header.component';
-
-// action de redux
-import { setCurrentUser } from './redux/user/user.actions';
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect, Route, Switch } from "react-router-dom"; // Redirect nos permite hacer que no se pueda acceder a la pagina sin logearse
+import { createStructuredSelector } from "reselect";
+import Header from "./components/header/header.component";
 // firebase
-import { auth, createUserProfileDocument, addCollectionsAndDocuments } from './firebase/firebase.utils';
-import { selectCollectionForPreview } from './redux/shop/shop.selector';
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { GlobalStyles } from "./global.styles";
+import CheckoutPage from "./pages/checkout/checkout.component";
+// Componentes
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+// action de redux
+import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
-
   /* Ya no se necesita mas el contructor porque seteamos user desde redux
     constructor() {
       super();
@@ -38,10 +26,9 @@ class App extends React.Component {
 
   unsubscribeFromAuth = null;
 
-
   componentDidMount() {
-    const { setCurrentUser  } = this.props;
-    
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -52,11 +39,10 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          })
+          });
         });
       }
       setCurrentUser(userAuth);
-
     });
   }
 
@@ -67,17 +53,19 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyles />
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route exact path='/signin' render={() =>
-            this.props.currentUser ? (
-              <Redirect to='/' />
-            ) : (
-                <SignInAndSignUp />)
-          } />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+            }
+          />
         </Switch>
       </div>
     );
@@ -90,7 +78,6 @@ const mapStateToProps = createStructuredSelector({
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
-
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
