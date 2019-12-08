842ff76f208691c8b469374b488492d31159d020
Pablo Garcia <pablo.garcia366@gmail.com>
2019-12-08 00:36:36 -0300

    estilos responsivos agregados

--- src/pages/homepage/homepage.style.jsx
+++ src/pages/homepage/homepage.style.jsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
-
 export const HomePageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 80px;
+  @media screen and (max-width: 800px) {
+    padding: 10px;
+  }
 `;
