f4f28bcaddfedde630bb02abf28648a009fb9e48
Pablo Garcia <48216252+pablogugarcia@users.noreply.github.com>
2020-01-19 19:38:47 -0300

    Delete clave.firebase.js

--- src/firebase/clave.firebase.js
+++
@@ -1,9 +0,0 @@
-export const claveFirebase = {
-    apiKey: "AIzaSyAr4aPDwwC2QaXKVNvDU0zlfTLa95_m2-Y",
-    authDomain: "ecommerce-ropa-db.firebaseapp.com",
-    databaseURL: "https://ecommerce-ropa-db.firebaseio.com",
-    projectId: "ecommerce-ropa-db",
-    storageBucket: "",
-    messagingSenderId: "416047173950",
-    appId: "1:416047173950:web:a7c5019fabff8842"
-};
